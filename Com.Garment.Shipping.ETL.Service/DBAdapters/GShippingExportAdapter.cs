95758d42bc5892d2c1d7e1329e10109bad4cb986
Ady Hermawan <ady.hermawan@moonlay.com>
2021-07-29 14:35:20 +0700

    fix first commit

--- Com.Garment.Shipping.ETL.Service/DBAdapters/GShippingExportAdapter.cs
+++ Com.Garment.Shipping.ETL.Service/DBAdapters/GShippingExportAdapter.cs
@@ -1,23 +1,24 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Com.Garment.Shipping.ETL.Service.Models;
 using Com.Garment.Shipping.ETL.Service.Services;
 
 namespace Com.Garment.Shipping.ETL.Service.DBAdapters
 {
     public class GShippingExportAdapter : IGShippingExportAdapter
     {
-        public Task GetData(IEnumerable<GShippingExport> models)
+        public Task GetData(IEnumerable<GShippingExportModel> models)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task LoadData(IEnumerable<GShippingExport> models)
+        public Task LoadData(IEnumerable<GShippingExportModel> models)
         {
             throw new System.NotImplementedException();
         }
     }
 
-    public interface IGShippingExportAdapter : IBaseAdapter<GShippingExport>
+    public interface IGShippingExportAdapter : IBaseAdapter<GShippingExportModel>
     {
         
     }
--- Com.Garment.Shipping.ETL.Service/ManualETL.cs
+++ Com.Garment.Shipping.ETL.Service/ManualETL.cs
@@ -14,7 +14,7 @@ namespace Com.Garment.Shipping.ETL.Service
     {
         [FunctionName("manual-etl")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");

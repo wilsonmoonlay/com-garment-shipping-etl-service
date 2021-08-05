8925e01b0f72c6c1ecfc6c37bdd8b67909cc078b
wilsonmoonlay <54935452+wilsonmoonlay@users.noreply.github.com>
2021-08-05 15:00:02 +0700

    Manual ETL Latest

--- Com.Garment.Shipping.ETL.Service.Test/Services/ManualETLTest.cs
+++ Com.Garment.Shipping.ETL.Service.Test/Services/ManualETLTest.cs
@@ -31,11 +31,11 @@ namespace Com.Garment.Shipping.ETL.Service.Test.Services
             request.Setup(x => x.Query["name"]).Returns("Test");
             request.Setup(x => x.Body).Returns(ms);
             request.Setup(x => x.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*"));
-            request.Setup(x => x.Headers["Authorization"]).Returns(new Microsoft.Extensions.Primitives.StringValues("1000"));
+            request.Setup(x => x.Headers["Authorization"]).Returns(new Microsoft.Extensions.Primitives.StringValues("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRldjIiLCJwcm9maWxlIjp7ImZpcnN0bmFtZSI6IlRlc3QiLCJsYXN0bmFtZSI6IlVuaXQiLCJnZW5kZXIiOiJNIiwiZG9iIjoiMjAxNy0wMi0xN1QxODozNToyMyswNzowMCIsImVtYWlsIjoiZGV2QHVuaXQudGVzdCJ9LCJwZXJtaXNzaW9uIjp7IlBJIjo3LCJQNyI6NywiUDQiOjcsIlA2Ijo3LCJQMyI6NywiUDEiOjcsIkI0Ijo2LCJVVC9VTklULzAxIjo3LCJDOSI6NywiRjEiOjYsIkI5Ijo2LCJBMiI6NywiRjIiOjcsIkIxIjo3fSwiaWF0IjoxNjI2MjI5NzczfQ.QmqENXn8w22scW1-lXDu0KbUI-CHSmc5WRgUmEaT_e4"));
 
-            var tokenPayload = new Mock<TokenPayloadExtractorService>();
-            
+            var tokenPayload = new Mock<TokenPayloadExtractorService>();            
             var mockJwt = new Mock<JwtSecurityTokenHandler>();
+            //mockJwt.VerifyAdd(x => x.ReadJwtToken("1000"));
             
 
             var mockShippingExport = new Mock<IGShippingExportService>();
@@ -43,8 +43,8 @@ namespace Com.Garment.Shipping.ETL.Service.Test.Services
 
             var logModel = new LogingETLModel(1, "Area", new DateTime(), "Test", true);
 
-            var mockLogging = new Mock<ILogingETLService>();
-            mockLogging.Setup(x => x.Update(new LogingETLModel(1, "Area", new DateTime(), "Test", true)));
+            var mockLogging = new Mock<ILogingETLService>();                       
+            mockLogging.Setup(x => x.Update(logModel)).Returns(It.IsAny<Task>());
 
 
             ManualETL service = new ManualETL(mockShippingExport.Object,mockShippingLocal.Object, mockLogging.Object);

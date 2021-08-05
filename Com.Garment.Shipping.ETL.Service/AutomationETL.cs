e376e618d603d3ca8fca6e4e2d25c032c3f06a03
Ady Hermawan <ady.hermawan@moonlay.com>
2021-08-05 15:20:00 +0700

    Automation ETL

--- Com.Garment.Shipping.ETL.Service/AutomationETL.cs
+++ Com.Garment.Shipping.ETL.Service/AutomationETL.cs
@@ -1,16 +1,95 @@
 using System;
+using System.Threading.Tasks;
+using Com.Garment.Shipping.ETL.Service.Models;
+using Com.Garment.Shipping.ETL.Service.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Com.Garment.Shipping.ETL.Service
 {
-    public static class AutomationETL
+    public class AutomationETL
     {
-        [FunctionName("automation-etl")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        private readonly ILogingETLService _logingETLService;
+        private readonly IGShippingExportService _gShippingExportService;
+        private readonly IGShippingLocalService _gShippingLocalService;
+
+        public AutomationETL(
+            ILogingETLService logingETLService,
+            IGShippingExportService gShippingExportService,
+            IGShippingLocalService gShippingLocalService
+        )
+        {
+            _logingETLService = logingETLService;
+            _gShippingExportService = gShippingExportService;
+            _gShippingLocalService = gShippingLocalService;
+        }
+
+        [FunctionName("automation-etl-batch-1")]
+        public async Task AutomationETLBatch1([TimerTrigger("0 0 12 1/1 * ? *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            try
+            {
+                await RunETL();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [FunctionName("automation-etl-batch-2")]
+        public async Task AutomationETLBatch2([TimerTrigger("0 0 20 1/1 * ? *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            try
+            {
+                await RunETL();
+            }
+            catch (Exception ex)
+            {
+                await GenerateLoging(false);
+                throw ex;
+            }
+        }
+
+        public async Task GenerateLoging(bool status)
+        {
+            var data = new LogingETLModel(
+                0,
+                "Garment Shipping",
+                DateTime.Now,
+                "SYSTEM",
+                status
+            );
+
+            await _logingETLService.Save(data);
+        }
+
+        public async Task RunETL()
+        {
+            try
+            {
+                var result = await _gShippingExportService.Get();
+                await _gShippingExportService.ClearData(result);
+                await _gShippingExportService.Save(result);
+
+                var resultLocal = await _gShippingLocalService.Get();
+                await _gShippingLocalService.Save(resultLocal);
+                await _gShippingLocalService.Save(resultLocal);
+
+                await GenerateLoging(true);
+            }
+            catch (Exception ex)
+            {
+                await GenerateLoging(false);
+                throw ex;
+            }
+
         }
     }
 }
